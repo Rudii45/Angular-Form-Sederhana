b5210d57320a37cebb701a6851c66dd8761765a5
Rudii45 <rudiprb45@gmail.com>
2023-11-25 19:36:33 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {FormControl} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+
+})
+export class AppComponent {
+
+toppings = new FormControl('');
+  toppingList: string[] = ['INDONESIA', 'AMERIKA', 'RUSIA', 'INDIA', 'KOREA', 'ARAB'];
+
+ constructor(private formBuilder:FormBuilder){}
+
+ profileForm = this.formBuilder.group({
+   Nama:[''],
+   Email:[''],
+   Alamat:[''],
+   Negara:[''],
+   Nomor:[''],
+   Tgl:[''],
+   Gender:[''],
+ });
+
+
+
+ saveForm(){
+   console.log('Form data is ', this.profileForm.value);
+
+ }
+
+}
